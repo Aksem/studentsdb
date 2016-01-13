7c11fa223f6a598e11d44b24fb5c599f6fadefa5
Aksem <aders1234@gmail.com>
2016-01-13 14:17:22 +0200

    Updated urls in url.py according to Django 1.9

--- students/views.py
+++ students/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from ..models import Student, Group
+from .models import Student, Group
 from datetime import datetime
 
 def students_list(request):
@@ -108,3 +108,36 @@ def students_edit(request, sid):
 
 def students_delete(request, sid):
     return HttpResponse('<h1>Delete Student %s</h1>' % sid)
+
+#Group views
+def groups_list(request):
+    groups = Group.objects.all()
+    #Order
+    order_by = request.GET.get('order_by','')
+    if order_by in ('title',):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse','') == '1':
+            groups = groups.reverse()
+    #paginate!
+    paginator = Paginator(groups, 1)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        groups = paginator.page(paginator.num_pages)
+    return render(request, 'students/groups_list.html',
+        {'groups': groups})
+
+def groups_add(request):
+    return HttpResponse('<h1>Group Add Form</h1>')
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Delete Group %s</h1>' % gid)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,33 +1,28 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.contrib import admin
-from .settings import MEDIA_ROOT, DEBUG
+from students import views as students_views
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Students urls
-    url(r'^$', 'students.views.students.students_list', name='home'),
-    url(r'^students/add/$', 'students.views.students.students_add',
+    url(r'^$', students_views.students_list, name='home'),
+    url(r'^students/add/$',  students_views.students_add,
          name='students_add'),
     url(r'^students/(?P<sid>\d+)/edit/$',
-         'students.views.students.students_edit',
+         students_views.students_edit,
          name='students_edit'),
     url(r'^students/(?P<sid>\d+)/delete/$',
-         'students.views.students.students_delete',
+         students_views.students_delete,
          name='students_delete'),
 
     # Groups urls
-    url(r'^groups/$', 'students.views.groups.groups_list', name='groups'),
-    url(r'^groups/add/$', 'students.views.groups.groups_add',
+    url(r'^groups/$', students_views.groups_list, name='groups'),
+    url(r'^groups/add/$',students_views.groups_add,
          name='groups_add'),
-    url(r'^groups/(?P<gid>\d+)/edit/$', 'students.views.groups.groups_edit',
+    url(r'^groups/(?P<gid>\d+)/edit/$', students_views.groups_edit,
          name='groups_edit'),
-    url(r'^groups/(?P<gid>\d+)/delete/$', 'students.views.groups.groups_delete',
+    url(r'^groups/(?P<gid>\d+)/delete/$', students_views.groups_delete,
          name='groups_delete'),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+]
 
-)
-if DEBUG:
-    # serve files from media folder
-    urlpatterns += patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
-            'document_root': MEDIA_ROOT}))
