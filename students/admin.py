8e40225e743389d3a77cddba78f7c130276f6eae
Aksem <aders1234@gmail.com>
2016-01-14 12:44:16 +0200

    Changed admin views for models.

--- students/admin.py
+++ students/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Student
+from .models import Student, Group
 # Register your models here.
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader']
+
 admin.site.register(Student)
+admin.site.register(Group, GroupAdmin)
